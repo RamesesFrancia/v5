e68c73d59c63fbd1726a68e79bb8a74990bef3aa
C:\Users\Rameses\Desktop\Version4\version <ramesesfrancia@gmail.com>
2017-01-24 03:55:56 +0800

    First Commit

---
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -0,0 +1,65 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-sms/www/SMS.js",
+        "id": "cordova-plugin-sms.SMS",
+        "pluginId": "cordova-plugin-sms",
+        "clobbers": [
+            "window.SMS"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-sms/www/SMS.js",
+        "id": "cordova-plugin-sms.SMS",
+        "pluginId": "cordova-plugin-sms",
+        "clobbers": [
+            "window.SMS"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-sms/www/SMS.js",
+        "id": "cordova-plugin-sms.SMS",
+        "pluginId": "cordova-plugin-sms",
+        "clobbers": [
+            "window.SMS"
+        ]
+    },
+    {
+        "file": "plugins/com.phonegap.plugins.PushPlugin/www/PushNotification.js",
+        "id": "com.phonegap.plugins.PushPlugin.PushNotification",
+        "pluginId": "com.phonegap.plugins.PushPlugin",
+        "clobbers": [
+            "PushNotification"
+        ]
+    },
+    {
+        "file": "plugins/onesignal-cordova-plugin/www/OneSignal.js",
+        "id": "onesignal-cordova-plugin.OneSignal",
+        "pluginId": "onesignal-cordova-plugin",
+        "clobbers": [
+            "OneSignal"
+        ]
+    },
+    {
+        "file": "plugins/onesignal-cordova-plugin-pgb-compat/www/OneSignal.js",
+        "id": "onesignal-cordova-plugin-pgb-compat.OneSignal",
+        "pluginId": "onesignal-cordova-plugin-pgb-compat",
+        "clobbers": [
+            "OneSignal"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-sms": "1.0.5",
+    "com.phonegap.plugins.PushPlugin": "2.5.0",
+    "cordova-plugin-compat": "1.1.0",
+    "cordova-plugin-geolocation": "2.4.1",
+    "onesignal-cordova-plugin": "2.0.11",
+    "onesignal-cordova-plugin-pgb-compat": "2.0.10",
+    "cordova-plugin-googleplayservices": "19.0.3",
+    "cordova-plugin-android-support-v4": "21.0.1"
+}
+// BOTTOM OF METADATA
+});
